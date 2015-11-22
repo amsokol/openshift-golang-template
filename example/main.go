7fb5f8af958223dd649abb2f6810cd6a5f4a2d91
Alexander Sokolovsky <amsokol@gmail.com>
2015-11-22 20:27:25 +0600

    refactored

--- example/main.go
+++ example/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo"
+	mw "github.com/labstack/echo/middleware"
+	"github.com/tylerb/graceful"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love golang!")
+func hello(c *echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!\n")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    fmt.Println("Starting server at port 8080...")
-    http.ListenAndServe(":8080", nil)
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(mw.Logger())
+	e.Use(mw.Recover())
+
+	// Routes
+	e.Get("/", hello)
+
+	// Setup server
+	port := "8080"
+	s := e.Server(":" + port)
+
+	// HTTP2 is currently enabled by default in echo.New(). To override TLS handshake errors
+	// you will need to override the TLSConfig for the server so it does not attempt to validate
+	// the connection using TLS as required by HTTP2
+	s.TLSConfig = nil
+
+	// Start server
+	log.Printf("Starting app on port %+v\n", port)
+	graceful.ListenAndServe(s, 5*time.Second)
 }
