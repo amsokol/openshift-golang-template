0d802496e80b63f50952f8ec1253234940b29c62
Aleksandr Sokolovskii <amsokol@gmail.com>
2017-05-31 10:52:00 +0300

    added govendor example project (#5)

---
+++ example-golang-govendor/pkg/cmd/server/healthz.go
@@ -0,0 +1,27 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/amsokol/openshift-golang-template/example-golang-govendor/pkg/fake"
+)
+
+// live provides HTTP endpoint for application liveness probe
+func live(w http.ResponseWriter, r *http.Request) {
+	s, err := fake.Live()
+	if err != nil {
+		w.WriteHeader(http.StatusServiceUnavailable)
+	} else {
+		w.Write([]byte(s))
+	}
+}
+
+// ready provides HTTP endpoint for application readiness probe
+func ready(w http.ResponseWriter, r *http.Request) {
+	s, err := fake.Ready()
+	if err != nil {
+		w.WriteHeader(http.StatusServiceUnavailable)
+	} else {
+		w.Write([]byte(s))
+	}
+}
