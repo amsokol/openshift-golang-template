8bc7df2e914e76ba421bff9fd2c06e1fe2be9d72
Aleksandr Sokolovskii <amsokol@gmail.com>
2017-05-31 10:33:19 +0300

    added glide example project (#5)

---
+++ example-golang-glide/pkg/cmd/server/hello.go
@@ -0,0 +1,12 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/amsokol/openshift-golang-template/example-golang-glide/pkg/fake"
+)
+
+// hello provides HTTP endpoint for "Hello" method
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fake.Hello()))
+}
