b684ef0e9e65160ac6a0e24945286ad318a77aa1
Aleksandr Sokolovskii <amsokol@gmail.com>
2017-06-13 19:00:30 +0300

    #8

--- example-godep/pkg/cmd/cli/params.go
+++ example-godep/pkg/cmd/cli/params.go
@@ -1,8 +1,18 @@
 package cli
 
 import (
+	"fmt"
 	"os"
 )
+
+// EchoArgs prints arguments are provided via command line
+func EchoArgs() {
+	if len(os.Args) > 1 {
+		fmt.Println("Command line arguments:", os.Args[1:])
+	} else {
+		fmt.Println("No command line arguments are provided")
+	}
+}
 
 // GetPort returns HTTP port to listen from environment variable value or default value 8080
 func GetPort() string {
